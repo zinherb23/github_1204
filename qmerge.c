96016239016e61cf9d416d250bfbf97a32bc4a94
zinherb23 <zinherb23@gmail.com>
2025-12-20 19:56:30 +0800

    mergeiterative

--- qmerge.c
+++ qmerge.c
@@ -5,7 +5,7 @@ void merge(int* arr, int* temp, int left, int mid, int right){
 	int m = mid+1;
 	int o = left;
 	while(n <= mid && m <= right){
-		if(arr[n] < arr[m]){
+		if(arr[n] > arr[m]){
 			temp[o++] = arr[n++];
 		}else{
 			temp[o++] = arr[m++];
@@ -21,25 +21,26 @@ void merge(int* arr, int* temp, int left, int mid, int right){
 		arr[i] = temp[i];
 	}
 }
-
-
-void merge_sort(int* arr, int* temp, int left, int right){
-	if(left >= right) return;
-	int mid = left + (right - left)/2;
-	merge_sort(arr, temp, left, mid);
-	merge_sort(arr, temp, mid+1, right);
-	merge(arr, temp, left, mid, right);
-}
-
-void merge_sort_temp(int* arr, int n){
+void merge_sort(int* arr, int n){
 	int temp[n];
-	merge_sort(arr, temp, 0, n-1);
+	for(int curr = 1; curr < n; curr*=2){
+		for(int left = 0; left < n -1; left += curr*2){
+			int mid = left + curr - 1;
+			int right = left + curr*2 - 1;
+			if(mid >= n){
+				continue;
+			}
+			if(right >= n){
+				right = n - 1;
+			}
+			merge(arr,temp,left, mid, right);
+		}
+	}	
 }
-
 int main(){
 	int arr[] = {5,2,4,1,6,3};
 	int n = sizeof(arr)/sizeof(arr[0]);
-	merge_sort_temp(arr,n);
+	merge_sort(arr,n);
 	for(int i = 0; i < n; i++){
 		printf("%d ",arr[i]);
 	}
